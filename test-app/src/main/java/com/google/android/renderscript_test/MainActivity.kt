3825c9651ac0d27a457191a5d1f5233cc125c3ae
Lize <lize@lize.tech>
2022-10-30 17:47:54 +0800

    Add publish script

--- test-app/src/main/java/com/google/android/renderscript_test/MainActivity.kt
+++ test-app/src/main/java/com/google/android/renderscript_test/MainActivity.kt
@@ -16,15 +16,48 @@
 
 package com.google.android.renderscript_test
 
+import android.graphics.BitmapFactory
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.android.renderscript.Range2d
+import com.google.android.renderscript.Toolkit
 
+private const val TAG = "MainActivity"
 
 @ExperimentalUnsignedTypes
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val begin = System.currentTimeMillis()
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img)
+        val radius = 25.0f
+
+        val blur = Toolkit.blur(bitmap, radius.toInt().coerceIn(1, 25))
+        findViewById<ImageView>(R.id.img).setImageBitmap(blur)
+        val end = System.currentTimeMillis()
+        Log.d(TAG, "onCreate: ${end - begin}")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val effect = RenderEffect.createBlurEffect(
+                radius,
+                radius,
+                Shader.TileMode.CLAMP
+            )
+
+            findViewById<ImageView>(R.id.img2).apply {
+                setImageBitmap(bitmap)
+                setRenderEffect(effect)
+            }
+        }
+
 
         // To debug resources not destroyed
         // "A resource failed to call destroy."
